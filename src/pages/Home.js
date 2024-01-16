84a4cb4ad2a7ce20741ff53793af29119db716fc
Benzazoua <billalben252@gmail.com>
2024-01-16 18:26:40 +0100

    Horizontal Scrollbar && Body Part Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import {Box} from '@mui/material'
+import React, { useState } from "react";
+import { Box } from "@mui/material";
 
-import HeroBanner from '../components/HeroBanner'
-import Exercises from '../components/Exercises'
-import SearchExercises from '../components/SearchExercises'
+import HeroBanner from "../components/HeroBanner";
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
