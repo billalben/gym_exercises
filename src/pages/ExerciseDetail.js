15784c1c37e6d23769ae4de78484f1cd06f3f404
Benzazoua <billalben252@gmail.com>
2024-01-19 11:13:08 +0100

    Exercises Detail

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+import SimilarExercises from "../components/SimilarExercises";
+import ExerciseVideos from "../components/ExerciseVideos";
+import Detail from "../components/Detail";
 
 const ExerciseDetail = () => {
-  return (
-    <div>ExerciseDetail</div>
-  )
-}
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
 
-export default ExerciseDetail
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUr =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`, exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData)
+    };
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
